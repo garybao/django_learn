ab589354592a6c5fc6c765af08a6e3ea4a11a914
Gary Bao <297204571@qq.com>
2016-03-30 18:42:55 +0800

    fun-with-forms

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,15 +5,17 @@ from django.http import HttpResponse
 #Import the Category model
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
+from rango.forms import PageForm
 
 def index(request):
 	context_dict = {}
 	#query the Category table order by likes in descending order.
 	#retrieve the top 5 only
-	category_list = Category.objects.order_by('-likes')[:5]
+	category_list = Category.objects.order_by('-likes')[:10]
 	#context_dict = {'categories':category_list}
 	context_dict['categories']=category_list
-	page_list = Page.objects.order_by('views')[:5]
+	page_list = Page.objects.order_by('views')[:10]
 	context_dict['pages'] = page_list
 	#context_dict = {'boldmessage': "I am bold font from the context"}
 	return render(request,'rango/index.html',context_dict)
@@ -38,3 +40,38 @@ def category(request, category_name_slug):
 	except Category.DoesNotExist:
 		pass
 	return render(request, 'rango/category.html',context_dict)
+
+def add_category(request):
+	if request.method =='POST':
+		form = CategoryForm(request.POST)
+
+		if form.is_valid():
+			form.save(commit=True)
+			return index(request)
+		else:
+			print form.errors
+	else:
+		form = CategoryForm()
+	return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+	try:
+		cat = Category.objects.get(slug=category_name_slug)
+	except Category.DoesNotExist:
+		cat = None 
+
+	if request.method == 'POST':
+		form = PageForm(request.POST)
+		if form.is_valid():
+			if cat:
+				page = form.save(commit=False)
+				page.category = cat
+				page.views = 0
+				page.save()
+				return category(request,category_name_slug)
+		else:
+			print form.errors
+	else:
+		form = PageForm()
+	context_dict = {'form':form, 'category': cat}
+	return render(request, 'rango/add_page.html',context_dict)
