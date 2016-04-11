914b8c6b7391c18145b032418cfaae43028d67e0
Gary Bao <297204571@qq.com>
2016-04-11 17:35:46 +0800

    19

--- tango_with_django_project/static/js/rango-jquery.js
+++ tango_with_django_project/static/js/rango-jquery.js
@@ -7,3 +7,12 @@ $(document).ready(function() {
 		alert("You clicked me! ouch!");
 	});
 });
+
+$('#likes').click(function(){
+	var catid;
+	catid = $(this).attr("data-catid");
+	$.get('/rango/like_category/', {category_id: catid}, function(data){
+		$('#like_count').html(data);
+		$('#likes').hide();
+	});
+});
